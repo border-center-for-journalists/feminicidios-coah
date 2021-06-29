63f0eb0af17adda4739c118d6f5a9c26e8647552
El Sonny <sonny@spaceshiplabs.com>
2021-06-29 18:28:59 +0200

    center headers

--- src/App.svelte
+++ src/App.svelte
@@ -5,10 +5,15 @@
 </script>
 
 <main>
-	<h1>Carpetas con resolucion por feminicidio</h1>
+	<h1>Carpetas con resolución por feminicídio</h1>
 	<DistrictChart />
-	<h1>Homicidios dolosos a mujeres vs carpetas iniciadas por feminicidio</h1>
+	<h1>Homicidios dolosos a mujeres vs carpetas iniciadas por feminicídio</h1>
 	<TimeGraph />
 	<h1>Homicidios a mujeres por lugar y causa de muerte</h1>
 	<SankeyChart />
 </main>
+<style>
+	h1{
+		text-align: center;
+	}
+</style>
